cd894a6e319883277730dfc8140cdbc1581f2534
everton <evertonsegur5@gmail.com>
2019-09-17 13:47:15 -0300

    Modified structure of Exceptions logs

---
+++ src/main/java/dev/codenation/logs/exception/users/UserExistsException.java
@@ -0,0 +1,11 @@
+package dev.codenation.logs.exception.users;
+
+import dev.codenation.logs.exception.AbstractExceptionMessage;
+
+public class UserExistsException extends AbstractExceptionMessage {
+
+    public UserExistsException(String message) {
+        super(message);
+    }
+
+}
