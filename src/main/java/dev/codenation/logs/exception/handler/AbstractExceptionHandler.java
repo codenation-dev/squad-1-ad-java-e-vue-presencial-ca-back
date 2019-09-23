9c6fc08b0dd2f5230c7f081a435c2e546d6cb302
Everton Segur <evertonsegur5@gmail.com>
2019-09-22 22:32:23 -0300

    fixando erros no pr

--- src/main/java/dev/codenation/logs/exception/handler/AbstractExceptionHandler.java
+++ src/main/java/dev/codenation/logs/exception/handler/AbstractExceptionHandler.java
@@ -4,14 +4,12 @@ import dev.codenation.logs.exception.message.AbstractExceptionMessage;
 import dev.codenation.logs.exception.message.model.AbstractNotFoundMessage;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @SuppressWarnings("ConstantConditions")
 @Order
-@ControllerAdvice
 public abstract class AbstractExceptionHandler<T extends AbstractExceptionMessage> extends ResponseEntityExceptionHandler {
 
     private T message;
--- src/main/java/dev/codenation/logs/exception/handler/log/LogExceptionHandler.java
+++ src/main/java/dev/codenation/logs/exception/handler/log/LogExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
