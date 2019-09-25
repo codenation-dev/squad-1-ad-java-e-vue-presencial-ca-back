e73a9efce6d443c7892fbcf8eba6bea40c1c50a7
Everton Segur <evertonsegur5@gmail.com>
2019-09-25 13:53:38 -0300

    creating auth with google api

--- src/main/java/dev/codenation/logs/authentication/WebSecurityConfig.java
+++ src/main/java/dev/codenation/logs/authentication/WebSecurityConfig.java
@@ -7,8 +7,10 @@ import dev.codenation.logs.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,6 +37,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         }
         auth.userDetailsService(userLogin -> new UserAuthVO(repository.findByEmail(userLogin)));
 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .mvcMatchers(HttpMethod.GET,
+                        "/",
+                        "/webjars/**",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/**",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/**/*.css",
+                        "/**/*.js");
     }
 
     @Bean
