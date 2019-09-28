ed4cda669450f834f2a196f0919f117fb917b694
Erica Peters <ericapetersc@gmail.com>
2019-09-28 13:46:11 -0300

    Add service tests

---
+++ src/test/java/dev/codenation/logs/service/UserServiceTest.java
@@ -0,0 +1,78 @@
+package dev.codenation.logs.service;
+
+import dev.codenation.logs.domain.entity.User;
+import dev.codenation.logs.repository.UserRepository;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService service;
+
+    @MockBean
+    private UserRepository repository;
+
+    @Test
+    public void WhenFindByValidId_ReturnUser() {
+        UUID id = UUID.randomUUID();
+        User userExpected = createUser();
+        userExpected.setId(id);
+        when(repository.findById(id)).thenReturn(Optional.of(userExpected));
+
+        Optional<User> userFound = service.findById(id);
+
+        assertThat(userFound.get(), Matchers.equalTo(userExpected));
+    }
+
+    @Test
+    public void WhenSaveUser_ReturnSameUser() {
+        UUID id = UUID.randomUUID();
+        User userExpected = createUser();
+        userExpected.setId(id);
+        when(repository.save(userExpected)).thenReturn(userExpected);
+
+        User userSaved = service.save(userExpected);
+
+        assertThat(userSaved, Matchers.equalTo(userExpected));
+    }
+
+    @Test
+    public void WhenFindAllUsers_ReturnAllUsers(){
+        User user1 = createUser();
+        User user2 = createUser();
+        User user3 = createUser();
+        List<User> users = Arrays.asList(user1, user2, user3);
+
+        when(repository.findAll()).thenReturn(users);
+
+        List<User> usersFound = repository.findAll();
+
+        assertThat(usersFound.size(), Matchers.equalTo(3));
+        assertThat(usersFound, Matchers.equalTo(users));
+    }
+
+    private User createUser(){
+        return User.builder()
+                .email("user@test.com")
+                .firstName("User")
+                .lastName("Test")
+                .password("Password")
+                .build();
+    }
+}
