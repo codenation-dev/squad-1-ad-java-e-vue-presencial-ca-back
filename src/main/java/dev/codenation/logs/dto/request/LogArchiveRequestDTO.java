cdfbc486a80dd4e862c9d0c561b60c35997c0cd5
Everton Segur <evertonsegur5@gmail.com>
2019-10-02 02:29:52 -0300

    fixed return uuid incorrect

--- src/main/java/dev/codenation/logs/dto/request/LogArchiveRequestDTO.java
+++ src/main/java/dev/codenation/logs/dto/request/LogArchiveRequestDTO.java
@@ -1,15 +1,24 @@
 package dev.codenation.logs.dto.request;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
 public class LogArchiveRequestDTO {
+
     @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "uuid2")
+    @GenericGenerator(name = "uuid2",strategy = "uuid2")
     private UUID id;
+
     @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "uuid2")
+    @GenericGenerator(name = "uuid2",strategy = "uuid2")
     private UUID userId;
     @NotNull
     private Boolean archived;
